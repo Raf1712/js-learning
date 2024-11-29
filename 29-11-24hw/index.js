76d4430796e2362a15e65a28eac32d9027d7a0ef
$Raf <rafvyan@gmail.com>
2024-11-29 23:59:12 +0400

    Homework tasks

--- 29-11-24hw/index.js
+++ 29-11-24hw/index.js
@@ -1,34 +1,47 @@
+// Problem 1: Student Grades
+// Description:
 // You are provided with an array of students, where each student is represented as an object. Each object contains the student’s name and their score. Your task is to determine the grade status of each student using a for loop and a switch statement.
 // The grade categories are as follows:
 // i >= 90 → “Excellent”
 // i >= 75 → “Good”
 // i >= 50 → “Pass”
 // default → “Fail”
+// Data example
+// const students = [
+//   { name: "Anna", grade: 85 },
+//   { name: "Mariam", grade: 92 },
+//   { name: "Olivia", grade: 76 },
+//   { name: "Emma", grade: 59 },
+// ];
 
 
+const students = [
+  { name: "Anna", grade: 85 },
+  { name: "Mariam", grade: 92 },
+  { name: "Olivia", grade: 76 },
+  { name: "Emma", grade: 59 },
+]; 
 
-
- 
-/* * /* const students = [
-    { nam: "Anna", grade: 85 },
-    { nam: "Mariam", grade: 92 },
-    { nam: "Olivia", grade: 76 },
-    { nam: "Emma", grade: 59 },
-    
-  ];  */
-  let output;
-  let i = 0;
-
-
-  switch(gradeStatus){
-    case('i >= 90') : output = 'Excellent'; break;
-    case('i >= 75') : output = 'Good'; break;
-    case('i >= 50') : output = 'Pass'; break;
-    default : output = 'Fail'; break;
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
   
+  switch (true) {
+    case (student.grade >= 90):
+      console.log(`${student.name}: Excellent`);
+      break;
+    case (student.grade >= 75):
+      console.log(`${student.name}: Good`);
+      break;
+    case (student.grade >= 50):
+      console.log(`${student.name}: Pass`);
+      break;
+    default:
+      console.log(`${student.name}: Fail`);
   }
+}
 
-  console.log(output)
+
+
   
   
 
