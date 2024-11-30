591cf7cc2dd693eedb58e4617111864faf0e7a36
$Raf <rafvyan@gmail.com>
2024-11-30 16:24:35 +0400

    problem 1

--- my-folder/index.js
+++ my-folder/index.js
@@ -59,44 +59,49 @@
 // ];
 
 
-const students = [
-    { name: "Anna", grade: 85 },
-    { name: "Mariam", grade: 92 },
-    { name: "Olivia", grade: 76 },
-    { name: "Emma", grade: 59 },
-  ]; 
+// const students = [
+//     { name: "Anna", grade: 85 },
+//     { name: "Mariam", grade: 92 },
+//     { name: "Olivia", grade: 76 },
+//     { name: "Emma", grade: 59 },
+//   ]; 
 
-for (let i = 0; i < students.length; i++) {
-    const student = students[i];
+// for (let i = 0; i < students.length; i++) {
+//     const student = students[i];
     
-    switch (true) {
-      case (student.grade >= 90):
-        console.log(`${student.name}: Excellent`);
-        break;
-      case (student.grade >= 75):
-        console.log(`${student.name}: Good`);
-        break;
-      case (student.grade >= 50):
-        console.log(`${student.name}: Pass`);
-        break;
-      default:
-        console.log(`${student.name}: Fail`);
+//     switch (true) {
+//       case (student.grade >= 90):
+//         console.log(`${student.name}: Excellent`);
+//         break;
+//       case (student.grade >= 75):
+//         console.log(`${student.name}: Good`);
+//         break;
+//       case (student.grade >= 50):
+//         console.log(`${student.name}: Pass`);
+//         break;
+//       default:
+//         console.log(`${student.name}: Fail`);
+//     }
+//   }
+
+
+
+// __________________________________________________________________________________________________________
+
+
+import { CARS } from "./cars.js";
+import { STUDENTS } from "./students.js";
+
+const safeCars = [];
+const speedingCars = [];
+
+for(let = 0; i < CARS.length; i++){
+    if(CARS[i].speed >= 120){
+        speedingCars.push(CARS[i]);
+    } else {
+        safeCars.push(CARS[i]);
     }
-  }
+}
 
-
-//   Problem 2: Car Speeds
-// Description:
-// You are given an array of cars, where each car has a model and speed (in km/h). Your task is to identify which cars are exceeding 120 km/h and which are safe (≤120 km/h). Store the safe cars and speeding cars in separate arrays. Use a for loop and a if else statement.
-// Data example
-// const cars = [
-//     { model: "Toyota", speed: 100 },
-//     { model: "BMW", speed: 150 },
-//     { model: "Lada", speed: 80 },
-//     { model: "Audi", speed: 130 },
-//     { model: "Mercedes-Benz", speed: 260 }
-// ];
-
-// const safeCars = [];
-// const speedingCars = [];
-
+console.log(safeCars, 'safeCars');
+console.log(speedingCars, 'speedingCars');
---
+++ my-folder/students.js
@@ -0,0 +1,6 @@
+export const students = [
+    { name: "Anna", grade: 85 },
+    { name: "Mariam", grade: 92 },
+    { name: "Olivia", grade: 76 },
+    { name: "Emma", grade: 59 },
+  ];
